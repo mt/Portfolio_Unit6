de6d51be696d96912e0f453a64a17720c7bf4b7b
Mathew Thomas <mathew.thomas+c9@gmail.com>
2016-12-20 21:54:31 +0000

    I updated my portfolio

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
+/* global $ */
 $(document).ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
       $("#like").html("You like this project!");
     });
-    
-    
-    
-    
 });
